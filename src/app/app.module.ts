178c2e0fe2a3de8298e7d8d778068826c735da14
Soen Surya Soenaryo <wei.surya@gmail.com>
2017-09-02 12:36:03 +0700

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -43,7 +44,8 @@ import { LoginComponent } from './login/login.component'
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [ DishService,
               PromotionService,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormGroupDirective } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedbackForm2: FormGroup;
+  feedback: Feedback;
+  feedback2: Feedback;
+  contactType = ContactType;
+  @ViewChild('f') feedbackFormDirective;
+  //@ViewChild('f2') feedbackFormDirective2;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();  
+  }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    /*this.feedbackForm2 = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });*/
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    //this.feedbackForm.reset();
+    this.feedbackFormDirective.resetForm();
+  }
+  
+  /*onSubmit2() {
+    this.feedback2 = this.feedbackForm2.value;
+    console.log(this.feedback2);
+    //this.feedbackForm.reset();
+    this.feedbackFormDirective2.resetForm();
+  }*/
 }
