db992de386f5fddfe11e43cb62819718ce385b35
Soen Surya Soenaryo <wei.surya@gmail.com>
2017-09-03 03:19:11 +0700

    Promise Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -8,15 +8,24 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+    return  new Promise(resolve => {
+      //Silumate server latency with 2 sec. delay
+      setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
 
   getPromotion(id: number): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
+    return  new Promise(resolve => {
+      //Silumate server latency with 2 sec. delay
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
+    return  new Promise(resolve => {
+      //Silumate server latency with 2 sec. delay
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000);
+    });
   }
 
 }
