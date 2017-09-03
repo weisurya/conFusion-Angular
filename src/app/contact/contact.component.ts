cb70c091c044274d1f50bddc49664b2465f4ae65
Soen Surya Soenaryo <wei.surya@gmail.com>
2017-09-03 15:26:27 +0700

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,34 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   feedback2: Feedback;
   contactType = ContactType;
+  formErrors = {
+    'firstname' : '',
+    'lastname' : '',
+    'telnum' : '',
+    'email' : ''
+  };
+  
+  validationMessages = {
+    'firstname': {
+      'required': 'First name is required.',
+      'minlength': 'First name must be at least 2 characters long.',
+      'maxlength': 'First name cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required': 'Last name is required.',
+      'minlength': 'Last name must be at least 2 characters long.',
+      'maxlength': 'Last name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    },
+  };
+
   @ViewChild('f') feedbackFormDirective;
   //@ViewChild('f2') feedbackFormDirective2;
 
@@ -26,14 +54,20 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); //(re)set form validation messages
+
     /*this.feedbackForm2 = this.fb.group({
       firstname: '',
       lastname: '',
@@ -43,6 +77,22 @@ export class ContactComponent implements OnInit {
       contacttype: 'None',
       message: ''
     });*/
+  }
+  onValueChanged(data?: any) {
+    if(!this.feedbackForm) {return;}
+
+    const form = this.feedbackForm;
+
+    for(const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if(control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   onSubmit() {
