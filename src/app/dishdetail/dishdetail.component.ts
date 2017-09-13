1714abda42eb117b9b54734d4dd1049ab2ba7f52
Soen Surya Soenaryo <wei.surya@gmail.com>
2017-09-14 04:04:32 +0700

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,13 +8,27 @@ import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 //import 'rxjs/add/operator/switchmap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -27,6 +41,7 @@ export class DishdetailComponent implements OnInit {
   next: number;
   comment: Comment;
   errMess: string;
+  visibility = 'shown';
 
   formErrors = {
     'author' : '',
@@ -63,11 +78,15 @@ export class DishdetailComponent implements OnInit {
     .subscribe(dishIds => this.dishIds = dishIds );
 
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+      .switchMap((params: Params) => { 
+        this.visibility = 'hidden';
+        return this.dishservice.getDish(+params['id']); 
+      })
       .subscribe(dish => {
         this.dish = dish;
         this.dishcopy = dish;
-        this.setPrevNext(dish.id)
+        this.setPrevNext(dish.id);
+        this.visibility = 'shown';
       },
       errmess => this.errMess = <any>errmess);
   }
